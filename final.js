51e77b06a1f3ea6b039778de033bb83f218fec07
Collin Selle <sellecollin@gmail.com>
2019-12-10 09:26:07 -0700

    Tuesday

--- final.js
+++ final.js
@@ -16,6 +16,12 @@ submit.onclick = function() {
         school_img.classList.add('hide');
     }
 }
+
+document.getElementById('region').onchange = function() {
+switch(document.getElementById('region') {
+}
+}
+
 function results(region, size, type) {
     if (region == 'east' && size == 'small' && type == 'private') {
         return 0;
@@ -126,51 +132,51 @@ function whatname(x) {
         case 6:
         return 'King University'  
         case 7:
-        return ''  
+        return 'Kentucky State University'  
         case 8:
-        return ''  
+        return 'Duke University'  
         case 9:
-        return ''  
+        return 'College of William and Mary'  
         case 10:
-        return ''  
+        return 'Keiser University'  
         case 11:
-        return ''  
+        return 'Auburn University'  
         case 12:
-        return ''  
+        return 'Lake Forest College'  
         case 13:
-        return ''      
+        return 'Mayville State University'      
         case 14:
-        return ''  
+        return 'Bradley University'  
         case 15:
-        return ''  
+        return 'Black Hills State University'  
         case 16:
-        return ''  
+        return 'Creighton University'  
         case 17:
-        return ''  
+        return 'University of Minnesota: Twin Cities'  
         case 18:
-        return ''  
+        return 'Amberton University'  
         case 19:
-        return ''  
+        return 'Western New Mexico University'  
         case 20:
-        return ''  
+        return 'Rice University'  
         case 21:
-        return ''  
+        return 'Arkansas Tech University'  
         case 22:
-        return ''  
+        return 'Texas Christian University'  
         case 23:
-        return ''  
+        return 'Texas A & M University'  
         case 24:
-        return ''  
+        return 'California Institute of Technology'  
         case 25:
-        return ''  
+        return 'Western State Colorado University'  
         case 26:
-        return ''  
+        return 'Gonzaga University'  
         case 27:
-        return ''  
+        return 'Colorado School of Mines'  
         case 28:
-        return ''  
+        return 'University of Southern California'  
         case 29:
-        return ''  
+        return 'University of Colorado Boulder'  
 }
 }
 
@@ -191,50 +197,50 @@ function whatimg(x) {
         case 6:
         return '<img src="https://secure-media.collegeboard.org/CollegePlanning/media/image/photo/3319_Photo.jpg" alt="" >'  
         case 7:
-        return '<img src="" alt="" >'  
+        return '<img src="https://secure-media.collegeboard.org/CollegePlanning/media/image/photo/3244_Photo.jpg" alt="" >'  
         case 8:
-        return '<img src="" alt="" >'  
+        return '<img src="https://secure-media.collegeboard.org/CollegePlanning/media/image/photo/535_Photo.jpg" alt="" >'  
         case 9:
-        return '<img src="" alt="" >'  
+        return '<img src="https://secure-media.collegeboard.org/CollegePlanning/media/image/photo/3090_Photo.jpg" alt="" >'  
         case 10:
-        return '<img src="" alt="" >'  
+        return '<img src="https://res.cloudinary.com/highereducation/image/upload/w_740,h_416,c_fill,f_auto,fl_lossy,q_auto/v1/TheBestSchools.org/keiser-university.jpg" alt="" >'  
         case 11:
-        return '<img src="" alt="" >'  
+        return '<img src="https://secure-media.collegeboard.org/CollegePlanning/media/image/photo/1608_Photo.jpg" alt="" >'  
         case 12:
-        return '<img src="" alt="" >'  
+        return '<img src="https://secure-media.collegeboard.org/CollegePlanning/media/image/photo/4238_Photo.jpg" alt="" >'  
         case 13:
-        return '<img src="" alt="" >'      
+        return '<img src="https://secure-media.collegeboard.org/CollegePlanning/media/image/photo/75_Photo.jpg" alt="" >'      
         case 14:
-        return '<img src="" alt="" >'  
+        return '<img src="https://secure-media.collegeboard.org/CollegePlanning/media/image/photo/1869_Photo.jpg" alt="" >'  
         case 15:
-        return '<img src="" alt="" >'  
+        return '<img src="https://www.bhsu.edu/portals/0/Images/Rooftop_4.jpg?ver=2018-01-05-184926-087" alt="" >'  
         case 16:
-        return '<img src="" alt="" >'  
+        return '<img src="https://secure-media.collegeboard.org/CollegePlanning/media/image/photo/2147_Photo.jpg" alt="" >'  
         case 17:
-        return '<img src="" alt="" >'  
+        return '<img src="https://collegesimply.firebaseapp.com/img/primary/university-of-minnesota-twin-cities-174066.jpg" alt="" >'  
         case 18:
-        return '<img src="" alt="" >'  
+        return '<img src="https://secure-media.collegeboard.org/CollegePlanning/media/image/photo/344_Photo.jpg" alt="" >'  
         case 19:
-        return '<img src="" alt="" >'  
+        return '<img src="https://secure-media.collegeboard.org/CollegePlanning/media/image/photo/3274_Photo.jpg" alt="" >'  
         case 20:
-        return '<img src="" alt="" >'  
+        return '<img src="https://secure-media.collegeboard.org/CollegePlanning/media/image/photo/2569_Photo.jpg" alt="" >'  
         case 21:
-        return '<img src="" alt="" >'  
+        return '<img src="https://secure-media.collegeboard.org/CollegePlanning/media/image/photo/404_Photo.jpg" alt="" >'  
         case 22:
-        return '<img src="" alt="" >'  
+        return '<img src="https://secure-media.collegeboard.org/CollegePlanning/media/image/photo/2438_Photo.jpg" alt="" >'  
         case 23:
-        return '<img src="" alt="" >'  
+        return '<img src="https://secure-media.collegeboard.org/CollegePlanning/media/image/photo/397_Photo.jpg" alt="" >'  
         case 24:
-        return '<img src="" alt="" >'  
+        return '<img src="https://secure-media.collegeboard.org/CollegePlanning/media/image/photo/4214_Photo.jpg" alt="" >'  
         case 25:
-        return '<img src="" alt="" >'  
+        return '<img src="https://secure-media.collegeboard.org/CollegePlanning/media/image/photo/3981_Photo.jpg" alt="" >'  
         case 26:
-        return '<img src="" alt="" >'  
+        return '<img src="https://www.gonzaga.edu/-/media/Website/Images/Wide-Header/About/Our-Campus-and-Location/wide-drone-hemmingson-header-image.ashx?h=600&w=1440&la=en&hash=F15FC462B8350E0FFE14304794B3770D411C2473" alt="" >'  
         case 27:
-        return '<img src="" alt="" >'  
+        return '<img src="https://secure-media.collegeboard.org/CollegePlanning/media/image/photo/1300_Photo.jpg" alt="" >'  
         case 28:
-        return '<img src="" alt="" >'  
+        return '<img src="https://secure-media.collegeboard.org/CollegePlanning/media/image/photo/3341_Photo.jpg" alt="" >'  
         case 29:
-        return '<img src="" alt="" >'  
+        return '<img src="https://secure-media.collegeboard.org/CollegePlanning/media/image/photo/994_Photo.jpg" alt="" >'  
 }
 }
